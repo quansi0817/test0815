32b85fde68270c6e714ff416b9292d404fdc4431
quansi0817 <quansi@sheridancollege.ca>
2023-08-15 12:05:00 -0400

    test

---
+++ src/test0815/Test0815.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package test0815;
+
+/**
+ *
+ * @author sichao
+ */
+public class Test0815 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+	public int getNumber() {
+		return this.number;
+	}
+
+	/**
+	 * 
+	 * @param number
+	 */
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+	public void getAttribute() {
+		// TODO - implement Test0815.getAttribute
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param attribute
+	 */
+	public void setAttribute(int attribute) {
+		this.attribute = attribute;
+	}
+
+	private int number;
+	private int attribute;
+    
+}
